fecb1dbafc2bbec8fa84d684030ade0cb4b2068e
Steven Chang <stevenchang5000@protonmail.com>
2024-02-22 15:27:00 +1000

    add start, end and whitelist duration (#2)

--- lib.rs
+++ lib.rs
@@ -8,6 +8,9 @@ mod az_token_sale_to_airdrop {
     pub struct Config {
         airdrop: AccountId,
         token: AccountId,
+        start: Timestamp,
+        end: Timestamp,
+        whitelist_duration: Timestamp,
     }
 
     // === CONTRACT ===
@@ -15,11 +18,26 @@ mod az_token_sale_to_airdrop {
     pub struct AzTokenSaleToAirdrop {
         airdrop: AccountId,
         token: AccountId,
+        start: Timestamp,
+        end: Timestamp,
+        whitelist_duration: Timestamp,
     }
     impl AzTokenSaleToAirdrop {
         #[ink(constructor)]
-        pub fn new(airdrop: AccountId, token: AccountId) -> Self {
-            Self { airdrop, token }
+        pub fn new(
+            airdrop: AccountId,
+            token: AccountId,
+            start: Timestamp,
+            end: Timestamp,
+            whitelist_duration: Timestamp,
+        ) -> Self {
+            Self {
+                airdrop,
+                token,
+                start,
+                end,
+                whitelist_duration,
+            }
         }
 
         // === QUERIES ===
@@ -28,6 +46,9 @@ mod az_token_sale_to_airdrop {
             Config {
                 airdrop: self.airdrop,
                 token: self.token,
+                start: self.start,
+                end: self.end,
+                whitelist_duration: self.whitelist_duration,
             }
         }
     }
@@ -40,11 +61,21 @@ mod az_token_sale_to_airdrop {
             DefaultEnvironment,
         };
 
+        const MOCK_START: Timestamp = 654_654;
+        const MOCK_END: Timestamp = 754_654;
+        const MOCK_WHITELIST_DURATION: Timestamp = 1_000;
+
         // === HELPERS ===
         fn init() -> (DefaultAccounts<DefaultEnvironment>, AzTokenSaleToAirdrop) {
             let accounts = default_accounts();
             set_caller::<DefaultEnvironment>(accounts.bob);
-            let az_token_sale_to_airdrop = AzTokenSaleToAirdrop::new(mock_airdrop(), mock_token());
+            let az_token_sale_to_airdrop = AzTokenSaleToAirdrop::new(
+                mock_airdrop(),
+                mock_token(),
+                MOCK_START,
+                MOCK_END,
+                MOCK_WHITELIST_DURATION,
+            );
             (accounts, az_token_sale_to_airdrop)
         }
 
@@ -66,6 +97,12 @@ mod az_token_sale_to_airdrop {
             // * it returns the config
             assert_eq!(config.airdrop, az_token_sale_to_airdrop.airdrop);
             assert_eq!(config.token, az_token_sale_to_airdrop.token);
+            assert_eq!(config.start, az_token_sale_to_airdrop.start);
+            assert_eq!(config.end, az_token_sale_to_airdrop.end);
+            assert_eq!(
+                config.whitelist_duration,
+                az_token_sale_to_airdrop.whitelist_duration
+            );
         }
     }
 }
