0a6d4a7e7c5d4925898a5a67d85bdd9ae8a51ce3
Steven Chang <stevenchang5000@protonmail.com>
2024-02-22 16:04:23 +1000

    fn whitelist_add

--- lib.rs
+++ lib.rs
@@ -1,11 +1,20 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
+mod errors;
+
 #[ink::contract]
 mod az_token_sale_to_airdrop {
+    use crate::errors::AzTokenSaleToAirdropError;
+    use ink::{prelude::string::ToString, storage::Mapping};
+
+    // === TYPES ===
+    type Result<T> = core::result::Result<T, AzTokenSaleToAirdropError>;
+
     // === STRUCTS ===
     #[derive(Debug, Clone, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub struct Config {
+        admin: AccountId,
         airdrop: AccountId,
         token: AccountId,
         start: Timestamp,
@@ -16,10 +25,12 @@ mod az_token_sale_to_airdrop {
     // === CONTRACT ===
     #[ink(storage)]
     pub struct AzTokenSaleToAirdrop {
+        admin: AccountId,
         airdrop: AccountId,
         token: AccountId,
         start: Timestamp,
         end: Timestamp,
+        whitelist: Mapping<AccountId, AccountId>,
         whitelist_duration: Timestamp,
     }
     impl AzTokenSaleToAirdrop {
@@ -32,10 +43,12 @@ mod az_token_sale_to_airdrop {
             whitelist_duration: Timestamp,
         ) -> Self {
             Self {
+                admin: Self::env().caller(),
                 airdrop,
                 token,
                 start,
                 end,
+                whitelist: Mapping::default(),
                 whitelist_duration,
             }
         }
@@ -44,12 +57,39 @@ mod az_token_sale_to_airdrop {
         #[ink(message)]
         pub fn config(&self) -> Config {
             Config {
+                admin: self.admin,
                 airdrop: self.airdrop,
                 token: self.token,
                 start: self.start,
                 end: self.end,
                 whitelist_duration: self.whitelist_duration,
             }
+        }
+
+        // === HANDLES ===
+        #[ink(message)]
+        pub fn whitelist_add(&mut self, address: AccountId) -> Result<AccountId> {
+            let caller: AccountId = Self::env().caller();
+            Self::authorise(caller, self.admin)?;
+
+            if self.whitelist.get(address).is_some() {
+                return Err(AzTokenSaleToAirdropError::UnprocessableEntity(
+                    "Already on whitelist".to_string(),
+                ));
+            } else {
+                self.whitelist.insert(address, &address);
+            }
+
+            Ok(address)
+        }
+
+        // === PRIVATE ===
+        fn authorise(allowed: AccountId, received: AccountId) -> Result<()> {
+            if allowed != received {
+                return Err(AzTokenSaleToAirdropError::Unauthorised);
+            }
+
+            Ok(())
         }
     }
 
@@ -95,6 +135,7 @@ mod az_token_sale_to_airdrop {
             let (_accounts, az_token_sale_to_airdrop) = init();
             let config = az_token_sale_to_airdrop.config();
             // * it returns the config
+            assert_eq!(config.admin, az_token_sale_to_airdrop.admin);
             assert_eq!(config.airdrop, az_token_sale_to_airdrop.airdrop);
             assert_eq!(config.token, az_token_sale_to_airdrop.token);
             assert_eq!(config.start, az_token_sale_to_airdrop.start);
@@ -103,6 +144,39 @@ mod az_token_sale_to_airdrop {
                 config.whitelist_duration,
                 az_token_sale_to_airdrop.whitelist_duration
             );
+        }
+
+        // === TEST HANDLES ===
+        #[ink::test]
+        fn test_whitelist_add() {
+            let (accounts, mut az_token_sale_to_airdrop) = init();
+            let new_address: AccountId = accounts.django;
+            // when called by admin
+            // = when address is not on whitelist
+            let mut result = az_token_sale_to_airdrop.whitelist_add(new_address);
+            result.unwrap();
+            // = * it adds the address to whitelist
+            assert_eq!(
+                az_token_sale_to_airdrop
+                    .whitelist
+                    .get(new_address)
+                    .is_some(),
+                true
+            );
+            // = when already on whitelist
+            // = * it raises an error
+            result = az_token_sale_to_airdrop.whitelist_add(new_address);
+            assert_eq!(
+                result,
+                Err(AzTokenSaleToAirdropError::UnprocessableEntity(
+                    "Already on whitelist".to_string()
+                ))
+            );
+            // when called by non admin
+            // * it raises an error
+            set_caller::<DefaultEnvironment>(accounts.charlie);
+            result = az_token_sale_to_airdrop.whitelist_add(new_address);
+            assert_eq!(result, Err(AzTokenSaleToAirdropError::Unauthorised));
         }
     }
 }
