2a5f27f43f8669ce2575aab114237b2a882fab5e
Steven Chang <stevenchang5000@protonmail.com>
2024-02-24 15:21:25 +1000

    Remove az airdrop from workspace

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@
 
 set -eu
 
-cargo +stable contract build --manifest-path az_airdrop/Cargo.toml --release
 cargo +stable contract build --manifest-path az_button/Cargo.toml --release
 cargo +stable contract build --release
